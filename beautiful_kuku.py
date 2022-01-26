8af154eb7bd47dbc1e7e97ac482417a4a675cc4c
mikirupann <sw.miki.0907@icloud.com>
2022-01-27 00:55:17 +0900

    beautiful_kuku修正完了
    終わりました

--- beautiful_kuku.py
+++ beautiful_kuku.py
@@ -1,7 +1,7 @@
 line = input("行数を入力してください: ")
 column = input("列数を入力してください: ")
 
-for number1 in range(1, int(line)):
-    for number2 in range(1, int(column)):
-        print(f" {number2} × {number1} = {number1 * number2} |", end="")
+for number1 in range(1, (int(line)) + 1):
+    for number2 in range(1, (int(column)) + 1):
+        print(f" {number2} × {number1} = {str(number1 * number2).rjust(3)}", end=" | ")
     print()
