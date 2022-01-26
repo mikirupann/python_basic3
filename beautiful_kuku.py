76efeee863c1be7b90f13aa1bd0269ea8d6a7451
mikirupann <sw.miki.0907@icloud.com>
2022-01-27 01:10:02 +0900

    beautiful_kuku再修正完了
    終わりました

--- beautiful_kuku.py
+++ beautiful_kuku.py
@@ -3,5 +3,5 @@ column = input("列数を入力してください: ")
 
 for number1 in range(1, (int(line)) + 1):
     for number2 in range(1, (int(column)) + 1):
-        print(f" {number2} × {number1} = {str(number1 * number2).rjust(3)}", end=" | ")
+        print(f" {number2} × {number1} = {str(number1 * number2).rjust(2)}", end=" | ")
     print()
