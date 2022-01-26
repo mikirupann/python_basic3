696442b5bf5f326d0fa71447da25a10611d5ce26
mikirupann <sw.miki.0907@icloud.com>
2022-01-26 23:45:30 +0900

    kuku_2修正完了
    私は味噌ラーメンが好きです

--- kuku_2.py
+++ kuku_2.py
@@ -3,5 +3,5 @@ column = input("列数を入力してください: ")
 
 for number1 in range(1, int(line) + 1):
     for number2 in range(1, int(column) + 1):
-        print(number2 * number1, end="")
+        print(number2 * number1, end=" ")
     print()
